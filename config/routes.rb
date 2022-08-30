8a1a5b4b23e388cceb29c79395eebcbf1033760e
ttershak <ttershak@gmail.com>
2022-08-30 12:42:23 +0300

    adding help and home pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   get 'statpages/home'
   get 'statpages/help'
+  
+  #get "help" => "statpages#help"
+  #get "home" => "statpages#home"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #root "statpages#home"
 end
